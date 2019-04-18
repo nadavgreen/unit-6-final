617f9add836a5516034d55f321154ed0f566011c
nadavgree <nadavgreen@pursuit.org>
2019-04-17 21:34:11 -0400

    created server

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const getApp = () => {
+	const app = require('express')();
+	app.use(require('body-parser').json())
+	app.use((req, res, next)=>{
+		res.header("Access-Control-Allow-Origin" , "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	    	res.header("Access-Control-Allow-Methods", "PUT, DELETE")
+		next()
+	});
+
+    	return app;
+}
+
+module.exports = {
+    getApp,
+}
